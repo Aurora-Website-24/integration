cce7051fb4d64f1a79ec40b74753f7a86cbee25c
Kirit0me <prabhavagarwal444@gmail.com>
2024-01-20 15:10:25 +0530

    developers page

--- frontend/src/pages/Developers.js
+++ frontend/src/pages/Developers.js
@@ -18,8 +18,9 @@ const Seniors = [Kiri, Kiri, Kiri]
 
 function JuniorCard( {member} ) {
     return (
-        <div className="w-2/3 p-6 m-auto">
-            <div className="flex flex-col align-middle rounded-3xl border-1 border-white bg-gradient-to-r from-slate-700 to-black text-white p-6">
+        <div className="w-5/6 p-6 m-auto backdrop-blur-sm">
+
+            <div className="flex flex-col align-middle rounded-3xl border-1 border-slate-300 hover:border-slate-400 bg-aurora-bg hover:bg-gradient-to-br from-stone-800 to-bg-black bg-bottom text-white p-6">
                 <div className="p-4"><img className="rounded-full w-24 h-24 m-auto" src={member.img}></img></div>
                 <h1 className="text-center text-2xl font-bold py-4">{member.name}</h1>
                 <div className="rounded-3xl border-white border-2 p-2">
@@ -30,14 +31,14 @@ function JuniorCard( {member} ) {
                     <a href ={member.git_link}><button className="p-2"><img src={gitlogo} alt="" /></button></a>
                 </div>
             </div>
-        </div>
+            </div>
     )
 }
 
 function SeniorCard( {member} ) {
     return (
-        <div className="w-2/3 p-6 m-auto">
-            <div className="flex flex-col rounded-3xl border-1 border-white bg-gradient-to-r from-slate-700 to-black text-white p-6">
+        <div className="w-5/6 p-6 m-auto backdrop-blur-sm">
+            <div className="flex flex-col rounded-3xl bg-aurora-bg border-1 border-slate-300 hover:border-slate-400 bg-bottom hover:bg-gradient-to-br from-stone-800 to-bg-black text-white p-6">
                 <div className="p-4"><img className="rounded-full w-24 h-24 m-auto" src={member.img}></img></div>
                 <h1 className="text-center text-2xl font-bold py-4">{member.name}</h1>
                 <div className="rounded-3xl border-white border-2 p-2">
@@ -48,7 +49,7 @@ function SeniorCard( {member} ) {
                     <a href ={member.git_link}><button className="p-2"><img src={gitlogo} alt="" /></button></a>
                 </div>
             </div>
-        </div>
+            </div>
     )
 }
 
@@ -58,13 +59,13 @@ export default function Developers() {
         <Nav />
         <div className="bg-black pt-16 bg-dev-bg">
             <div className="p-10"><img src={meetjr} className="m-auto"></img></div>
-            <div className="grid md:grid-cols-3 grid-cols-1 p-6">
+            <div className="grid md:grid-cols-3 grid-cols-1 ">
             {Juniors.map((member) => (
                 <JuniorCard key={member.id} member={member} />
             ))}
             </div>
             <div className="p-10"><img src={meetsr} className="m-auto"></img></div>
-            <div className="grid md:grid-cols-3 grid-cols-1 p-6">
+            <div className="grid md:grid-cols-3 grid-cols-1 ">
             {Seniors.map((member) => (
                 <SeniorCard key={member.id} member={member} />
             ))}
